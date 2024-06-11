4467d6104a52d655223b8339b59173925e3ee6ce
Samuel <samuelint@gmail.com>
2024-06-11 08:46:53 -0400

    chore: run repository

---
+++ langchain_openai_api_bridge/assistant/repository/assistant_run_repository.py
@@ -0,0 +1,37 @@
+from openai.types.beta.threads import Run
+from abc import ABC, abstractmethod
+from typing import List, Optional
+from openai.types.beta.threads.run import RequiredAction, RunStatus, AssistantTool
+
+
+class AssistantRunRepository(ABC):
+
+    def __init__(
+        self,
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def create(
+        self,
+        assistant_id: str,
+        thread_id: str,
+        model: str,
+        status: RunStatus,
+        instructions: str = "",
+        required_action: Optional[RequiredAction] = None,
+        tools: List[AssistantTool] = [],
+    ) -> Run:
+        pass
+
+    @abstractmethod
+    def update(self, run: Run) -> Run:
+        pass
+
+    @abstractmethod
+    def retreive(self, thread_id: str) -> Run:
+        pass
+
+    @abstractmethod
+    def delete(self, run: Optional[Run], run_id: Optional[str]) -> None:
+        pass
