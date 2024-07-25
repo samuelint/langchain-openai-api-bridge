47d30d6b5369a30bccca74cfc99f1718dc543cb9
Samuel <samuelint@gmail.com>
2024-07-25 13:11:23 -0400

    fix: remove tool usage from groq test

--- tests/test_functional/fastapi_assistant_agent_groq/my_groq_agent_factory.py
+++ tests/test_functional/fastapi_assistant_agent_groq/my_groq_agent_factory.py
@@ -2,16 +2,9 @@ from langchain_groq import ChatGroq
 from langchain_openai_api_bridge.core.agent_factory import AgentFactory
 from langchain_core.runnables import Runnable
 from langchain_core.language_models import BaseChatModel
-from langchain_core.tools import tool
 from langgraph.prebuilt import create_react_agent
 
 from langchain_openai_api_bridge.core.create_agent_dto import CreateAgentDto
-
-
-@tool
-def magic_number_tool(input: int) -> int:
-    """Applies a magic function to an input."""
-    return input + 2
 
 
 class MyGroqAgentFactory(AgentFactory):
@@ -19,7 +12,7 @@ class MyGroqAgentFactory(AgentFactory):
     def create_agent(self, llm: BaseChatModel, dto: CreateAgentDto) -> Runnable:
         return create_react_agent(
             llm,
-            [magic_number_tool],
+            [],
             messages_modifier="""You are a helpful assistant.""",
         )
 
