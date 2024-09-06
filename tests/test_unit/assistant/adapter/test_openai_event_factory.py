37336e369b7c3dae25d24fbb1469fdcfc9af6357
Samuel <samuelint@gmail.com>
2024-09-06 09:06:14 -0400

    fix: test

--- tests/test_unit/assistant/adapter/test_openai_event_factory.py
+++ tests/test_unit/assistant/adapter/test_openai_event_factory.py
@@ -1,6 +1,8 @@
+import json
 from langchain_openai_api_bridge.assistant.adapter.openai_event_factory import (
     create_langchain_function,
 )
+from langchain_core.messages.tool import ToolMessage
 
 
 class TestCreateLangchainFunction:
@@ -20,3 +22,31 @@ class TestCreateLangchainFunction:
         result = create_langchain_function(arguments={"a": "a"}, output=2.1)
 
         assert result.output == "2.1"
+
+    def test_ToolMessageOutput_is_serialized_to_json(self):
+        tool_message_output = ToolMessage(
+            content="some",
+            tool_call_id="123",
+        )
+        result = create_langchain_function(
+            arguments={"a": "a"}, output=tool_message_output
+        )
+
+        output = json.loads(result.output)
+        assert output["content"] == "some"
+        assert output["tool_call_id"] == "123"
+        assert output["status"] == "success"
+        assert output.get("artifact") is None
+
+    def test_ToolMessageOutput_with_artifact_is_serialized_to_json(self):
+        tool_message_output = ToolMessage(
+            content="some",
+            tool_call_id="123",
+            artifact={"test": "test"},
+        )
+        result = create_langchain_function(
+            arguments={"a": "a"}, output=tool_message_output
+        )
+
+        output = json.loads(result.output)
+        assert output["artifact"] == {"test": "test"}
